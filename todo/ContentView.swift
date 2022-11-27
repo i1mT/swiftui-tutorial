e92ddb31b875d9a6353060912c524032e9d42fe2
i1mT <tfhhh@qq.com>
2022-11-27 21:24:05 +0800

    Initial Commit

---
+++ todo/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  todo
+//
+//  Created by 陶逢华 on 2022/11/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ todo/todoApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoApp.swift
+//  todo
+//
+//  Created by 陶逢华 on 2022/11/27.
+//
+
+import SwiftUI
+
+@main
+struct todoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
